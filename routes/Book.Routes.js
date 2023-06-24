8a38ff43424958921660cbbb042d445bd05c2cb3
janani <jayaramanjanani73@gmail.com>
2023-06-25 00:01:46 +0530

    added changes

--- routes/Book.Routes.js
+++ routes/Book.Routes.js
@@ -96,11 +96,6 @@ bookRouter.delete("/delete/:id", async (req, res) => {
 });
 
 
-bookRouter.get("/get/id",async(req,res)=>{
-
-  const book=await BookModel.find()
-  res.send(book)
-})
 
 
 module.exports = { bookRouter };
